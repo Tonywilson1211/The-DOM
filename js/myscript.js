f07b5f271c0058ddb9a694f560860bad6e5d8a71
Tonywilson1211 <ant.wilson@hotmail.com>
2023-04-10 16:17:40 +0000

    jquery

---
+++ js/myscript.js
@@ -0,0 +1,4 @@
+$(document).ready(function() { //ready function ensures the function only runs when the DOM is fully loaded
+    $("h2").addClass("underline"); //underlines all <h2> elements 
+    $("ul").addClass("border"); // adds a border to all the lists 
+})
